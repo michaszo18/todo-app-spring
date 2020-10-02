b6f1831544dc07653066fbd2eaaa162d3bc22381
Michal Szostek <michal.szostek@savangard.com>
2020-10-02 12:05:44 +0200

    wlasne property w application.yml zmiana sciezki

--- src/main/java/com/example/todoapp/TaskConfigurationProperties.java
+++ src/main/java/com/example/todoapp/TaskConfigurationProperties.java
@@ -1,17 +1,32 @@
 package com.example.todoapp;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @ConfigurationProperties("task")
 public class TaskConfigurationProperties {
 
-    private boolean allowMultipleTasksFromTemplate;
+    private Template template;
 
-    public boolean isAllowMultipleTasksFromTemplate() {
-        return allowMultipleTasksFromTemplate;
+    public Template getTemplate() {
+        return template;
     }
 
-    public void setAllowMultipleTasksFromTemplate(boolean allowMultipleTasksFromTemplate) {
-        this.allowMultipleTasksFromTemplate = allowMultipleTasksFromTemplate;
+    public void setTemplate(final Template template) {
+        this.template = template;
+    }
+
+    public static class Template {
+
+        private boolean allowMultipleTasks;
+
+        public boolean isAllowMultipleTasks() {
+            return allowMultipleTasks;
+        }
+
+        public void setAllowMultipleTasks(boolean allowMultipleTasks) {
+            this.allowMultipleTasks = allowMultipleTasks;
+        }
     }
 }
--- src/main/java/com/example/todoapp/controller/InfoController.java
+++ src/main/java/com/example/todoapp/controller/InfoController.java
@@ -1,7 +1,6 @@
 package com.example.todoapp.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import com.example.todoapp.TaskConfigurationProperties;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class InfoController {
 
-    @Autowired
     private DataSourceProperties dataSource;
-    @Value("${task.allowMultipleTasksFromTemplate}")
-    private boolean myProp;
+    private TaskConfigurationProperties myProp;
+
+
+    public InfoController(DataSourceProperties dataSource, TaskConfigurationProperties myProp) {
+        this.dataSource = dataSource;
+        this.myProp = myProp;
+    }
 
     @GetMapping("/info/url")
     String url() {
@@ -21,7 +24,7 @@ public class InfoController {
 
     @GetMapping("/info/prop")
     boolean myProp() {
-        return myProp;
+        return myProp.getTemplate().isAllowMultipleTasks();
     }
 
 }
