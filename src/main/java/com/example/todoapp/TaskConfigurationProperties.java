bfe384b6ab7b3592f26b71ff0134309c9147d5ba
Michal Szostek <michal.szostek@savangard.com>
2020-10-02 11:31:37 +0200

    wlasne property w application.yml

---
+++ src/main/java/com/example/todoapp/TaskConfigurationProperties.java
@@ -0,0 +1,17 @@
+package com.example.todoapp;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("task")
+public class TaskConfigurationProperties {
+
+    private boolean allowMultipleTasksFromTemplate;
+
+    public boolean isAllowMultipleTasksFromTemplate() {
+        return allowMultipleTasksFromTemplate;
+    }
+
+    public void setAllowMultipleTasksFromTemplate(boolean allowMultipleTasksFromTemplate) {
+        this.allowMultipleTasksFromTemplate = allowMultipleTasksFromTemplate;
+    }
+}
---
+++ src/main/java/com/example/todoapp/controller/InfoController.java
@@ -0,0 +1,27 @@
+package com.example.todoapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InfoController {
+
+    @Autowired
+    private DataSourceProperties dataSource;
+    @Value("${task.allowMultipleTasksFromTemplate}")
+    private boolean myProp;
+
+    @GetMapping("/info/url")
+    String url() {
+        return dataSource.getUrl();
+    }
+
+    @GetMapping("/info/prop")
+    boolean myProp() {
+        return myProp;
+    }
+
+}
