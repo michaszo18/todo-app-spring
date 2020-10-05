4269f293b952cc33454926dcdcda171af2dc052a
Michal Szostek <michal.szostek@savangard.com>
2020-10-05 12:01:38 +0200

    osadzenie klasy dat

--- src/main/java/com/example/todoapp/model/Audit.java
+++ src/main/java/com/example/todoapp/model/Audit.java
@@ -1,12 +1,12 @@
 package com.example.todoapp.model;
 
-import javax.persistence.MappedSuperclass;
+import javax.persistence.Embeddable;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import java.time.LocalDateTime;
 
-@MappedSuperclass
-abstract class BaseAuditableEntity {
+@Embeddable
+public class Audit {
 
     private LocalDateTime createdOn;
     private LocalDateTime updatedOn;
--- src/main/java/com/example/todoapp/model/Task.java
+++ src/main/java/com/example/todoapp/model/Task.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "tasks")
-public class Task extends BaseAuditableEntity {
+public class Task {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,6 +18,8 @@ public class Task extends BaseAuditableEntity {
     private String description;
     private boolean done;
     private LocalDateTime deadline;
+    @Embedded
+    private Audit audit = new Audit();
 
 
     public Task() {
